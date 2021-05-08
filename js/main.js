f4b9ddf4415650ee8fa9fa40f3f141ff793e6472
Martin Ade <2723616@gmail.com>
2021-05-08 12:20:03 -0400

    fade-up on scroll

--- js/main.js
+++ js/main.js
@@ -56,6 +56,7 @@ function main() {
                 queue: false
             }
         });
+        AOS.refresh();
         $('.cat a').click(function() {
             $('.cat .active').removeClass('active');
             $(this).addClass('active');
@@ -68,10 +69,11 @@ function main() {
                     queue: false
                 }
             });
+            AOS.refresh();
             return false;
         });
 
-
+        
     });
 	
 
@@ -81,6 +83,7 @@ function main() {
         keyboardNav: true,
     });
 
+    AOS.refresh();
 
 }());
 
@@ -131,6 +134,7 @@ $(".img-responsive").on("click", function() {
         $('#link').attr('href',doc);
         $('.modal').modal('show');
     }
+    AOS.refresh();
 });
 
 
@@ -140,9 +144,34 @@ $('.modal').on('hidden.bs.modal', function () {
     $('#video').removeClass('iframe-ux');
     $('#video').removeClass('iframe-website');
     $('#video').removeClass('iframe-video');
+    AOS.refresh();
 });
 
 
+AOS.init({
+   // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 0, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+
+AOS.refresh();
 
 
 // particlesJS.load('js/particles-js', 'css/particles.json', function() {
@@ -263,3 +292,4 @@ particlesJS('particles-js',
 }
 
 );
+
