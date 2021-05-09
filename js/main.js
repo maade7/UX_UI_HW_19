c98709e5bbfd717ed8a4d0347ea6d00577e206f8
Martin Ade <2723616@gmail.com>
2021-05-09 04:44:18 -0400

    delay

--- js/main.js
+++ js/main.js
@@ -147,6 +147,8 @@ $('.modal').on('hidden.bs.modal', function () {
 });
 
 
+
+// Scroll reveal...............................
 AOS.init({
    // Global settings:
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
@@ -161,7 +163,7 @@ AOS.init({
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
-  delay: 100, // values from 0 to 3000, with step 50ms
+  delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
